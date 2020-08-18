0333c0b6541b8e66b45283c65dff506265ac11d5
Stone Kim <Mon8Cats@gmail.com>
2020-08-18 00:07:49 -0500

    re-commit

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public HomeController(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
+        {
+            _employeeRepository = employeeRepository;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            return View(model);
+        }
+
+        
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details"
+            };
+         
+            return View(homeDetailsViewModel);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = ProcessUploadedFile(model);
+
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.Add(newEmployee);
+
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistngPhotoPath = employee.PhotoPath
+            };
+
+            return View(employeeEditViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+                if(model.Photo != null)
+                {
+                    if(model.ExistngPhotoPath != null)
+                    {
+                        string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", model.ExistngPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+                    employee.PhotoPath = ProcessUploadedFile(model);
+                }
+
+
+                _employeeRepository.Update(employee);
+
+                return RedirectToAction("details", new { id = employee.Id });
+            }
+
+            return View();
+        }
+
+        private string ProcessUploadedFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.Photo != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using(var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Photo.CopyTo(fileStream);
+                }
+                
+            }
+
+            return uniqueFileName;
+        }
+    }
+}
---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    Department = Dept.IT,
+                    Email = "mary@mail.com"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "John",
+                    Department = Dept.HR,
+                    Email = "john@mail.com"
+                },
+                new Employee
+                {
+                    Id = 3,
+                    Name = "Steve",
+                    Department = Dept.HR,
+                    Email = "steve@mail.com"
+                }
+            );
+        }
+    }
+}
